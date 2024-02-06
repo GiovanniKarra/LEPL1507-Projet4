c39a1f2a5efb7279c6eea73460c81a9b5dab3ef8
Iss25 <issambre.lhermite@student.uclouvain.be>
2024-02-06 12:02:54 +0100

    upgrade function skeleton

--- spherical_satellites_repartition.py
+++ spherical_satellites_repartition.py
@@ -1,2 +1,16 @@
 def spherical_satellites_repartition(N_satellites, cities_coordinates, cities_weight):
-	pass
+	"""
+    Calcule sur le plan sphérique la répartition des satellites optimale pour obtenir une couverture de villes maximale.
+
+    Arguments:
+    ---------
+    N_satellites: nombre de satellites à répartir.
+    cities_coordinates: coordonnées des villes prise en compte.
+    cities_weight: poids de chaque ville (ex: population).
+    
+    Returns:
+    --------
+    satellites_coordinates: numpy array contenant la valeur du signal Y, produit de la convolution, aux abscisses ny.
+    """
+	satellites_coordinates = ...
+	return satellites_coordinates
