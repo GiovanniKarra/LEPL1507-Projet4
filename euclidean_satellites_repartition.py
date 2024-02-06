b9ece259df9a89b996126a8e5ce6a142eca86206
Giovanni Karra <karragiovanni@gmail.com>
2024-02-06 09:35:25 +0100

    création des modules python

---
+++ euclidean_satellites_repartition.py
@@ -0,0 +1,2 @@
+def euclidean_satellites_repartition(N_satellites, cities_coordinates, cities_weight):
+	pass
---
+++ spherical_satellites_repartition.py
@@ -0,0 +1,2 @@
+def spherical_satellites_repartition(N_satellites, cities_coordinates, cities_weight):
+	pass
