821fd34bb537a832c04dd1173cd3fb2bfd4357ec
Iss25 <issambre.lhermite@student.uclouvain.be>
2024-02-06 12:46:12 +0100

    module visualise

---
+++ visualisation_dataset.py
@@ -0,0 +1,23 @@
+import plotly.express as px
+import pandas as pd
+
+def visu():
+    df = pd.read_csv("geonames_cleared.csv",delimiter=";")
+
+    df["latitude"] = df["Coordinates"].str.split(",",expand=True)[0].astype(float)
+    df["longitude"] = df["Coordinates"].str.split(",",expand=True)[1].astype(float)
+    
+    fig = px.scatter_mapbox(
+    df,
+    lat=df["latitude"],
+    lon=df["longitude"],
+    hover_name= df["Name"], # column added to hover information
+    mapbox_style="open-street-map",
+    # color_discrete_sequence=px.colors.qualitative.Dark24,
+    )
+
+    fig.show()
+    
+
+visu()
+print("done")
